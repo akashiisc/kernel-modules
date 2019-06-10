210ae8bd86ec876737646c478bd3a191aef9a040
Akash Panda <akashpanda@iisc.ac.in>
2019-06-10 23:05:04 +0530

    included pid in the dump log

--- page_map_stats/page_map_stats.c
+++ page_map_stats/page_map_stats.c
@@ -25,7 +25,7 @@
 #include <linux/kprobes.h>
 #include <linux/page-flags.h>
 #include <linux/huge_mm.h>
-
+#include <uapi/linux/kernel-page-flags.h>
 #define LICENSE "GPL"
 #define AUTHOR "Akash Panda"
 #define DESC "Get information regarding a page"
@@ -39,11 +39,8 @@ struct anon_vma_chain * (*anon_vma_interval_tree_iter_next_p)(struct anon_vma_ch
 
 struct anon_vma * (*page_get_anon_vma_p)(struct page *);
 
-static int pfn_address=0;
-module_param(pfn_address,int,0660);
-
-static char *printstring = "Technicality Inside ";
-module_param(printstring , charp, 0000);
+static unsigned long pfn=0;
+module_param(pfn,ulong,S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 
 struct file *file;
 
@@ -61,7 +58,7 @@ static bool is_stack(struct vm_area_struct *vma)
 static bool is_heap(struct vm_area_struct *vma_1)
 {
     return vma_1->vm_start <= vma_1->vm_mm->brk &&
-                        vma_1->vm_end >= vma_1->vm_mm->start_brk;
+        vma_1->vm_end >= vma_1->vm_mm->start_brk;
 }
 
 static bool is_file_backed(struct vm_area_struct *vma_1) {
@@ -72,43 +69,82 @@ struct page * get_page_from_pfn(unsigned long pfn) {
     return pfn_to_page(pfn);
 }
 
+void inspect_page(struct page *page) {
+
+}
+
 static int __init technicalityinside_init(void) {
-//    trace_printk(KERN_ALERT "Technicality inside : PID : %lld" , pid);
-//    trace_printk(KERN_ALERT "Print String: %s" , printstring);
-    unsigned long pfn = 0x6b4673;
-    
+    if(!pfn_valid(pfn)) {
+        trace_printk("READINGS_PAGEMAP: INVALID_PFN %lx\n" , pfn);
+        return;
+    } 
     struct page *page = get_page_from_pfn(pfn);
     if(page == NULL) {
+        trace_printk("READINGS_PAGEMAP: NULL_PAGE %lx\n" , pfn);
         return;
     }
-    page_get_anon_vma_p = (void *) kallsyms_lookup_name("page_get_anon_vma");
+    void* virtual = NULL;
+    virtual = page_address(page);
+    bool free_page = false;
+    bool anon_page = false;
+    bool unevictable_page = false;
+    bool unmovable_page = false;
+    if(PageAnon(page)) {
+        anon_page = true;
+    }
+    if(PageBuddy(page)) {
+        free_page = true;
+    }
+    else if(PageCompound(page)) {
 
-    struct anon_vma *anon_vma = page_get_anon_vma_p(page);
-    pgoff_t pgoff_start, pgoff_end;
- //   pgoff_start = page_to_pgoff(page);
-//    pgoff_end = pgoff_start + hpage_nr_pages(page) - 1;
-    struct anon_vma_chain *avc;
-    anon_vma_interval_tree_iter_first_p = (void *) kallsyms_lookup_name("anon_vma_interval_tree_iter_first");
-    anon_vma_interval_tree_iter_next_p = (void *) kallsyms_lookup_name("anon_vma_interval_tree_iter_next");
-    
-    for (avc = anon_vma_interval_tree_iter_first_p(&anon_vma->rb_root, 0, ULONG_MAX); avc ; avc = anon_vma_interval_tree_iter_next_p(avc, 0 , ULONG_MAX)) {
-
-//    anon_vma_interval_tree_foreach(avc, &anon_vma->rb_root,
-//			pgoff_start, pgoff_end) {
-		struct vm_area_struct *vma = avc->vma;
-        if(is_stack(vma)) {
-            trace_printk( "READINGS:  %lx %lx [stack]\n" , vma->vm_start , vma->vm_end);        
-        } else if (is_heap(vma)) {
-            trace_printk( "READINGS:  %lx %lx [heap]\n" , vma->vm_start , vma->vm_end);
-        } else if (is_file_backed(vma)) {
-            trace_printk( "READINGS:  %lx %lx [file_backed]\n" , vma->vm_start , vma->vm_end);
-        } else {
-            trace_printk( "READINGS:  %lx %lx [pata nai]\n" , vma->vm_start , vma->vm_end);
+    }
+    else if(!PageLRU(page)) {
+        if(__PageMovable(page) && !PageIsolated(page)) {
+            //This has to be a movable page.
         }
-        
-	}
-    //                trace_printk( "READINGS: %lx %lx %d C\n" , vm_start + ((unsigned long)1 << 12)*num_my , pfn_value + ((unsigned long)1 << 12)*num_my , page_type);
-    //}
+        else {
+            //This has to be an unmovable page.
+            unmovable_page = true;
+        }
+    }
+    else{
+    }
+    if(!anon_page && unmovable_page) {
+        trace_printk("READINGS_PAGEMAP: UNMOVEABLE_PAGE %lx\n" , pfn);
+        return;
+    } else if(anon_page){
+        page_get_anon_vma_p = (void *) kallsyms_lookup_name("page_get_anon_vma");
+
+        struct anon_vma *anon_vma = page_get_anon_vma_p(page);
+        pgoff_t pgoff_start, pgoff_end;
+        //   pgoff_start = page_to_pgoff(page);
+        //    pgoff_end = pgoff_start + hpage_nr_pages(page) - 1;
+        struct anon_vma_chain *avc;
+        anon_vma_interval_tree_iter_first_p = (void *) kallsyms_lookup_name("anon_vma_interval_tree_iter_first");
+        anon_vma_interval_tree_iter_next_p = (void *) kallsyms_lookup_name("anon_vma_interval_tree_iter_next");
+
+        for (avc = anon_vma_interval_tree_iter_first_p(&anon_vma->rb_root, 0, ULONG_MAX); avc ; avc = anon_vma_interval_tree_iter_next_p(avc, 0 , ULONG_MAX)) {
+
+            //    anon_vma_interval_tree_foreach(avc, &anon_vma->rb_root,
+            //			pgoff_start, pgoff_end) {
+            struct vm_area_struct *vma = avc->vma;
+            pid_t pid_process  = vma->vm_mm->owner->pid;
+            if(is_stack(vma)) {
+                trace_printk( "READINGS_PAGEMAP: %s %lx %d %lx %lx [stack]\n" , (unmovable_page?"KERNEL_PAGE":"USER_PAGE"), pfn , pid_process , vma->vm_start , vma->vm_end);        
+            } else if (is_heap(vma)) {
+                trace_printk( "READINGS_PAGEMAP: %s %lx %d %lx %lx [heap]\n" , (unmovable_page?"KERNEL_PAGE":"USER_PAGE"), pfn , pid_process , vma->vm_start , vma->vm_end);
+            } else if (is_file_backed(vma)) {
+                trace_printk( "READINGS_PAGEMAP: %s %lx %d %lx %lx [file_backed]\n" ,(unmovable_page?"KERNEL_PAGE":"USER_PAGE"), pfn , pid_process , vma->vm_start , vma->vm_end);
+            } else {
+                trace_printk( "READINGS_PAGEMAP: %s %lx %d %lx %lx [pata nai]\n" , (unmovable_page?"KERNEL_PAGE":"USER_PAGE"), pfn , pid_process , vma->vm_start , vma->vm_end);
+            }
+
+        }
+        //                trace_printk( "READINGS: %lx %lx %d C\n" , vm_start + ((unsigned long)1 << 12)*num_my , pfn_value + ((unsigned long)1 << 12)*num_my , page_type);
+        //}
+    } else {
+        trace_printk( "READINGS_PAGEMAP: UNKNOWN_PAGE %lx\n" , pfn);
+    }
     return 0;
 }
 
@@ -118,7 +154,7 @@ static int __init technicalityinside_init(void) {
 
 static void __exit technicalityinside_exit(void) {
     printk(KERN_ALERT "Module Exit : Technicality inside\n");
-//    file_close(file);
+    //    file_close(file);
 }
 
 module_init(technicalityinside_init);
