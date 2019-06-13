ebe431e0b6bdca32290b8e255019f9d218dfe012
Akash Panda <akashpanda@iisc.ac.in>
2019-06-13 11:48:26 +0530

    changes for getting file name

--- page_map_stats/page_map_stats.c
+++ page_map_stats/page_map_stats.c
@@ -136,8 +136,9 @@ static int __init technicalityinside_init(void) {
             } else if (is_file_backed(vma)) {
                 char path_file[1024];
                 path_file[0] = '\0';
-                d_path(&(vma->vm_file->f_path) , path_file , 1024);
-                trace_printk( "READINGS_PAGEMAP: %s %lx %d %lx %lx [file_backed] %s\n" ,(unmovable_page?"KERNEL_PAGE":"USER_PAGE"), pfn , pid_process , vma->vm_start , vma->vm_end , path_file);
+		char *returned_ptr;
+                returned_ptr = d_path(&(vma->vm_file->f_path) , path_file , 1024);
+                trace_printk( "READINGS_PAGEMAP: %s %lx %d %lx %lx [file_backed] %s\n" ,(unmovable_page?"KERNEL_PAGE":"USER_PAGE"), pfn , pid_process , vma->vm_start , vma->vm_end , returned_ptr);
             } else {
                 trace_printk( "READINGS_PAGEMAP: %s %lx %d %lx %lx [pata_nai]\n" , (unmovable_page?"KERNEL_PAGE":"USER_PAGE"), pfn , pid_process , vma->vm_start , vma->vm_end);
             }
