039725bcd210b4e91e5f76edae5bf76ef28c0e6b
Akash Panda <akashpanda@iisc.ac.in>
2019-07-14 21:54:14 +0530

    stack added changes

--- page_map_stats_ioctl/page_map_stats.c
+++ page_map_stats_ioctl/page_map_stats.c
@@ -103,8 +103,11 @@ static bool is_stack(struct vm_area_struct *vma)
 	 * its "stack".  It's not even well-defined for programs written
 	 * languages like Go.
 	 */
-	return vma->vm_start >= vma->vm_mm->start_stack &&
+
+	return vma->vm_start <= vma->vm_mm->start_stack &&
 		vma->vm_end >= vma->vm_mm->start_stack;
+	//return vma->vm_start >= vma->vm_mm->start_stack &&
+	//	vma->vm_end >= vma->vm_mm->start_stack;
 }
 
 static bool is_code(struct vm_area_struct *vma)
